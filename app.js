9c016b21acf60b0ee76614a75eafa74749e46931
modulartable <77018198+modulartable@users.noreply.github.com>
2022-08-24 11:03:33 -0400

    Portfolio v2
    Updated the portfolio with new skills, projects, and reworded the about me section.  Updated the styling and favicon.

--- app.js
+++ app.js
@@ -1,49 +1,44 @@
 const hamburger = document.getElementById("hamburger");
-const navBar = document.getElementById("navBar")
+const navBar = document.getElementById("navBar");
 const line1 = document.getElementById("line1");
 const line2 = document.getElementById("line2");
 const line3 = document.getElementById("line3");
 
-
-//Defining the options for our intersection observer 
+//Defining the options for our intersection observer
 let options = {
-root: null,
-rootMargin: "40px",
-threshold: 0.5
+  root: null,
+  rootMargin: "40px",
+  threshold: 0.5,
 };
 
 //A callback function that toggles the animations when an element is in the viewport
 const handleIntersect = (entries, observer) => {
-entries.forEach(entry => {
-entry.target.classList.toggle("isInViewport", entry.isIntersecting)
-})
-}
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("isInViewport", entry.isIntersecting);
+  });
+};
 
 //Creates a new intersection observer with the callback function above
-let observer= new IntersectionObserver(handleIntersect);
+let observer = new IntersectionObserver(handleIntersect);
 
 //Selects all elements in the DOM with the inviewport data attribute
-const elementsInView = document.querySelectorAll('[data-inviewport]');
-
+const elementsInView = document.querySelectorAll("[data-inviewport]");
 
 //Cycles through all selected elements and adds an observe listener to each
-elementsInView.forEach(el => {
-observer.observe(el, options);
-})
+elementsInView.forEach((el) => {
+  observer.observe(el, options);
+});
 
-//Toggle the mobile hamburger nav animations onClick 
+//Toggle the mobile hamburger nav animations onClick
 const animateBurger = () => {
+  //Toggles the transition classes on each line of the hamburger
+  line1.classList.toggle("line-1-transition");
+  line2.classList.toggle("line-2-transition");
+  line3.classList.toggle("line-3-transition");
 
-
-//Toggles the transition classes on each line of the hamburger
-   line1.classList.toggle("line-1-transition");
-    line2.classList.toggle("line-2-transition");
-    line3.classList.toggle("line-3-transition");
-
-
-//Toggles the navBar to display block 
-navBar.classList.toggle("navBarToggle");
+  //Toggles the navBar to display block
+  navBar.classList.toggle("navBarToggle");
 };
 
 //Listen for clicks on the hambuger nav to trigger the animate function
-hamburger.addEventListener('click', animateBurger);
+hamburger.addEventListener("click", animateBurger);
